1b6d2d1e78b90a0d54c5edd9b57cbac598ba2177
Mathieu Gauthron <mathieu@matlux.net>
2024-07-22 10:45:15 +0000

    init commit

---
+++ scripts/docker-build.sh
@@ -0,0 +1,1 @@
+docker build -t simple-ubuntu -f Dockerfile-osmosis-node .
---
+++ scripts/docker-run.sh
@@ -0,0 +1,1 @@
+docker run -v /home/ubuntu/mathieu/snapshots:/root/snapshots -p 26656:26656 -p 26657:26657 -it simple-ubuntu
